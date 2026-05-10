bac6a962f8ff5a25bb59970dfacc6996c5fa5d47
kaka <lokaarziki@gmail.com>
2026-05-10 14:51:23 +0700

    perbaikan frontend

--- frontend/lib/api/cart.ts
+++
@@ -1,9 +0,0 @@
-import { CartPayload } from "@/types/cart"
-
-export const CartAPI = {
-  add: async (payload: CartPayload) => {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(payload), 500)
-    })
-  },
-}
